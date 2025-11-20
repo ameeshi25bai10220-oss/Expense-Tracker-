c0d664ef33b4e443d97fe5b6da6f84396c292bdc
AM <ameeshi.25bai10220@vitbhopal.ac.in>
2025-11-21 00:48:24 +0530

    module2_data_processing.py

--- module2_data_processing.py
+++ module2_data_processing.py
@@ -18,16 +18,16 @@ def calculate_summary(expenses: List[Expense]) -> Dict[str, Any]:
     overspent = remaining_budget < 0
 
     amount_by_category: Dict[str, float] = {}
-    for exp in expenses:
+    for exp in expenses:  #loops through all expenses and adds them to the corresponding category
         amount_by_category[exp.category] = amount_by_category.get(exp.category, 0.0) + exp.amount
 
-    now = datetime.datetime.now()
+    now = datetime.datetime.now()  #to get the current date and time
     days_in_month = calendar.monthrange(now.year, now.month)[1]
-    remaining_days = days_in_month - now.day + 1
+    remaining_days = days_in_month - now.day + 1  # +1 is done to ensure that the calculation includes the current day 
     
     daily_budget = 0.0
     if remaining_days > 0:
-        daily_budget = remaining_budget / remaining_days
+        daily_budget = remaining_budget / remaining_days  #Checks if there are any days left. If so, it calculates the Required Daily Budget by dividing the remaining_budget by the remaining_days
     
     return {
         'total_spent': total_spent,
@@ -39,7 +39,7 @@ def calculate_summary(expenses: List[Expense]) -> Dict[str, Any]:
     }
 
 def detect_overspending(summary: Dict[str, Any]):
-    if summary['overspent']:
+    if summary['overspent']:  #Checks the overspent flag. If true, it triggers an overspending alert
         print(red(f"\n🚨 OVERSPENDING ALERT! You are Rs{abs(summary['remaining_budget']):.2f} over your Rs{BUDGET:.2f} budget!"))
-    elif summary['daily_budget'] < 0:
+    elif summary['daily_budget'] < 0:  #If the user is not yet over budget but the calculated daily_budget is negative, it means they have spent too fast and must now stop spending to avoid exceeding the budget. This triggers a red WARNING
          print(red(f"\n⚠️ WARNING: Your required daily budget is negative! Need to stop spending."))
